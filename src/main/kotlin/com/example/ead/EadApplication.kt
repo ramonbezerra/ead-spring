326e30fb5605bcf619c1d365a3fb9dbdcf0375cb
ramonbezerra <ramon.bezerra@outlook.com>
2023-09-03 00:18:29 -0300

    feat: initial structure

---
+++ src/main/kotlin/com/example/ead/EadApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ead
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EadApplication
+
+fun main(args: Array<String>) {
+	runApplication<EadApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ead/EadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ead
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
